73117e4bd927f73b739d3572edd93163e6521f0c
gitelmanmarcelo <marcelo.gitelman@gmail.com>
2023-04-16 23:57:20 +0300

    commit

--- fmh-client/src/serverUrl.js
+++ fmh-client/src/serverUrl.js
@@ -1,4 +1,4 @@
 // export const serverUrl = "http://10.100.102.15:5000"
-export const serverUrl = "http://localhost:5000"
+// export const serverUrl = "http://localhost:5000"
 // export const serverUrl = "https://find-my-home.onrender.com"; 
-// export const serverUrl = "https://find-my-home-online.onrender.com"; 
+export const serverUrl = "https://find-my-home-online.onrender.com"; 
